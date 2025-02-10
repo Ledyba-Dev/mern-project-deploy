d7f6fb50a66b64d36d706f7415cae7278bad0ac6
Rodrigo Vizcarra <rodrigovizcarra9623@gmail.com>
2025-02-10 12:30:30 -0300

    Cambios verce json back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist" // La carpeta de salida debe ser "dist"
-  }
+  plugins: [react()]
 })
